8669861cfbef4df3bed25fb18714bf28f4c40f34
ARJUN-SABU <arjunsabu99@gmail.com>
2022-12-07 18:25:13 +0530

    Added the basic router and controller setup

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
+const tasks = require("./routes/tasks");
 
 const PORT = 5500;
 app.listen(PORT, () => {
   console.log("Server is up and running on port " + PORT);
 });
+
+//middlewares
+app.use(express.json());
+
+app.use("/api/v1/tasks/", tasks);
 
 app.get("/hello", (req, res) => {
   res.send("Hello this is the task manager!");
