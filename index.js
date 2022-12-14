88af45ac9a25cd7286038a348f625b4759649c7d
ARJUN-SABU <arjunsabu99@gmail.com>
2022-12-14 20:29:56 +0530

    Added the custom not-found middleware

--- index.js
+++ index.js
@@ -2,21 +2,16 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const { connectToDb, getDbConnection } = require("./db/connect");
+const notFound = require("./middleware/not-found");
 
 //middlewares
 app.use(express.json());
 
 const PORT = 3000;
 let db;
-connectToDb((err) => {
-  if (!err) {
-    app.listen(PORT, () => {
-      console.log("Server is up and running on port " + PORT);
-    });
-    db = getDbConnection();
-  } else {
-    console.log(err);
-  }
+
+app.get("/", (req, res) => {
+  res.send("Hello this is the task manager!");
 });
 
 //add db object as a property to
@@ -33,6 +28,20 @@ app.use(
   tasks
 );
 
-app.get("/", (req, res) => {
-  res.send("Hello this is the task manager!");
+//we have used another middleware above
+//that is, when the url starts with
+//'/api/v1/tasks', then the above middleware
+//will run, and if it is any other url, then
+//this not found middleware will run.
+app.use(notFound);
+
+connectToDb((err) => {
+  if (!err) {
+    app.listen(PORT, () => {
+      console.log("Server is up and running on port " + PORT);
+    });
+    db = getDbConnection();
+  } else {
+    console.log(err);
+  }
 });
